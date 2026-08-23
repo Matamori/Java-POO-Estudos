593261efbeb5f8ab2d5bd1d437b70b6623bbb400
Mateus Amorim <164431875+Matamori@users.noreply.github.com>
2026-08-23 14:30:17 -0300

    Delete Veiculo.java

--- Veiculo.java
+++
@@ -1,9 +0,0 @@
-package modelo;
-public class Veiculo {
-    public String placa;
-    public String modelo;
-    public int ano;
-    public String fabricante;
-    public String cor;
-    public Cliente cliente;
-}
