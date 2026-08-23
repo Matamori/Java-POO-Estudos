dfcd639e46f9fd9676092fdbd4e161300dc84238
Mateus <mateus96amorim@gmail.com>
2026-08-23 13:45:11 -0300

    Aula 4 - Pacotes e associação entre objetos

---
+++ src/Veiculo.java
@@ -0,0 +1,9 @@
+package modelo;
+public class Veiculo {
+    public String placa;
+    public String modelo;
+    public int ano;
+    public String fabricante;
+    public String cor;
+    public Cliente cliente;
+}
---
+++ src/modelo/Cliente.java
@@ -0,0 +1,10 @@
+package modelo;
+
+public class Cliente {
+    public String cpf;
+    public String nome;
+    public String endereco;
+    public String telefone;
+    public String email;
+    public Veiculo veiculo;
+}
